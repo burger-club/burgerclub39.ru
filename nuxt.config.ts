2eab403011e7ddf698df9debfc5f1eb30fb3d356
Larsonicus <66951637+Larsonicus@users.noreply.github.com>
2022-05-20 01:35:48 +0300

    chore: init

---
+++ nuxt.config.ts
@@ -0,0 +1,36 @@
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+  target: 'static',
+  ssr: false,
+
+  head: {
+    title: 'Burger Club',
+    link: [
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
+    meta: [
+      { charset: 'utf-8' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+  },
+
+  css: ['virtual:windi.css', 'virtual:windi-devtools'],
+
+  plugins: [],
+
+  modules: ['nuxt-windicss'],
+
+  windicss: {
+    analyze: true,
+  },
+})
---
+++ windi.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from '@windicss/plugin-utils'
+import colors from 'windicss/colors'
+
+export default defineConfig({
+  /**
+   * Write windi classes in html attributes.
+   * @see https://windicss.org/features/attributify.html
+   */
+  attributify: true,
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: {
+        50: '#F5F9FF',
+        100: '#EBF3FE',
+        200: '#CEE0FD',
+        300: '#B1CDFB',
+        400: '#76A8F9',
+        500: '#3B82F6',
+        DEFAULT: '#3B82F6',
+        600: '#3575DD',
+        700: '#2C62B9',
+        800: '#234E94',
+        900: '#1D4079',
+      },
+      black: '#000',
+      white: '#fff',
+      blue: colors.sky,
+      green: colors.green,
+      red: colors.red,
+      rose: colors.rose,
+      yellow: colors.amber,
+      orange: colors.orange,
+      gray: colors.gray,
+      purple: colors.purple,
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Poppins',
+      },
+      transitionProperty: {
+        height: 'height',
+      },
+    },
+  },
+})
