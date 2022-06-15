d6b80bb90cd17d4bd2037857bacd72e460e290f4
SocketSomeone <socket.someone@gmail.com>
2022-06-15 20:38:06 +0200

    feat: init seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,43 @@ export default defineNuxtConfig({
           content: 'width=device-width, initial-scale=1',
         },
         { hid: 'description', name: 'description', content: '' },
+        {
+          name: 'keywords',
+          content: '',
+        },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'Necord' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://necord.org/img/preview.png',
+        },
+        {
+          hid: 'og:image:secure_url',
+          property: 'og:image:secure_url',
+          content: 'https://necord.org/img/preview.png',
+        },
+        {
+          hid: 'og:image:alt',
+          property: 'og:image:alt',
+          content: 'Necord',
+        },
+        { hid: 'twitter:site', name: 'twitter:site', content: '@socketsomeone' },
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: 'https://necord.org/img/preview.png',
+        },
+        {
+          hid: 'twitter:image:alt',
+          name: 'twitter:image:alt',
+          content: 'Necord',
+        },
       ],
     },
   },
