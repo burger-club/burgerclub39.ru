6125075aea7cfe0da4967d2a8acab39eec1e1a4e
Pavel Fedorin <fedorin.pasha59@gmail.com>
2022-06-23 05:38:31 +0300

    fix: crossorigin preconnect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       link: [
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap',
